d768ea5b001f48508c2b5792574b9c96172a44e9
Matias Quezada <m.quezada.loyola@gmail.com>
2023-12-08 17:56:31 -0300

    Add configuration for ejs

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello from YelpCamp!');
+    res.render('home');
 })
