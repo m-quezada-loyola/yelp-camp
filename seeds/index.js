178cab62104d8d91d1f6ff82a489da1d3ec270f0
Matias Quezada <m.quezada.loyola@gmail.com>
2023-12-09 02:52:40 -0300

    Populate database with URL images and display images on show page

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let index = 0; index < 50; index++) {
         const randomNumber = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const newCamp = new Campground({
             location: `${cities[randomNumber].city}, ${cities[randomNumber].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/random?forest_house',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatem illo nemo delectus nesciunt ad voluptatibus ex explicabo, in beatae aliquam vitae molestias, eligendi tempore sunt quibusdam? Ducimus, fugit accusamus!',
+            price
         })
         await newCamp.save();
     }
