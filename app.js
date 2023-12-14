4366593e22e147e4cdccc8f4a73a09715e09f397
Matias Quezada <m.quezada.loyola@gmail.com>
2023-12-14 16:59:50 -0300

    Add express-session for session support

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
+const session = require('express-session');
 const ExpressError = require('./utils/ExpressError');
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
@@ -12,6 +13,16 @@ const reviews = require('./routes/reviews')
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
 const app = express();
+const sessionConfig = {
+    secret: 'temporalsecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -19,6 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
+app.use(session(sessionConfig));
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews)
 
