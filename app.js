694b7a464e5ea9879315ba64b6d3985b6f336964
Matias Quezada <m.quezada.loyola@gmail.com>
2023-12-15 00:34:05 -0300

    Configure passport for authentication

--- app.js
+++ app.js
@@ -7,9 +7,12 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const ExpressError = require('./utils/ExpressError');
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
+const User = require('./models/user')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -40,6 +43,12 @@ app.use((req, res, next) => {
 })
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews)
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
