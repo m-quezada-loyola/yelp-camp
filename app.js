61e33c1f739cbe905de150fb36515380f0b3eceb
Matias Quezada <m.quezada.loyola@gmail.com>
2023-12-22 15:53:06 -0300

    Use db atlas URL to deploy

--- app.js
+++ app.js
@@ -20,8 +20,7 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/user');
 const User = require('./models/user');
-// const dbUrl =  process.env.DB_URL;
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl);
 
