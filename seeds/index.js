891ba3a69b6a8863779a29f26bd8ec9e2d262489
Matias Quezada <m.quezada.loyola@gmail.com>
2024-01-02 01:09:28 -0300

    Update default image url and campground quantity

--- seeds/index.js
+++ seeds/index.js
@@ -15,11 +15,11 @@ db.once('open', () => {
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let index = 0; index < 300; index++) {
+    for (let index = 0; index < 20; index++) {
         const randomNumber = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const newCamp = new Campground({
-            author: '657bd9a737397724ed0e742a',
+            author: '6591dcb5a4dd3ac88adc2435',
             location: `${cities[randomNumber].city}, ${cities[randomNumber].state}`,
             geometry: {
                 type: "Point",
@@ -31,12 +31,12 @@ const seedDB = async () => {
             title: `${sample(descriptors)} ${sample(places)}`,
             images: [
                 {
-                  url: 'https://res.cloudinary.com/dpvtarguc/image/upload/v1703024889/YelpCamp/whflf0xz1rm2eiphayan.jpg',
-                  filename: 'YelpCamp/whflf0xz1rm2eiphayan'
+                  url: 'https://res.cloudinary.com/dpvtarguc/image/upload/v1703959442/YelpCamp/a6vjrvvnv4t835wii4zx.jpg',
+                  filename: 'YelpCamp/a6vjrvvnv4t835wii4zx'
                 },
                 {
-                  url: 'https://res.cloudinary.com/dpvtarguc/image/upload/v1703024890/YelpCamp/rwpx17rn2qja2klkil5q.jpg',
-                  filename: 'YelpCamp/rwpx17rn2qja2klkil5q'
+                  url: 'https://res.cloudinary.com/dpvtarguc/image/upload/v1703959442/YelpCamp/nkdmhlyl5grwljvmtzya.jpg',
+                  filename: 'YelpCamp/nkdmhlyl5grwljvmtzya'
                 }
               ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus voluptatem illo nemo delectus nesciunt ad voluptatibus ex explicabo, in beatae aliquam vitae molestias, eligendi tempore sunt quibusdam? Ducimus, fugit accusamus!',
